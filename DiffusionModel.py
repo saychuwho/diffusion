159b836c0e7ba476a1056505ab5a692700d3d7ec
SeongJae Chu-main computer <saychuwho@dgist.ac.kr>
2024-07-23 11:53:25 +0900

    updated DDIM sampling process

--- DiffusionModel.py
+++ DiffusionModel.py
@@ -20,15 +20,18 @@ def cosine_beta_schedule(timesteps, s=0.008):
     betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
     return torch.clip(betas, 0.0001, 0.9999)
 
+
 def linear_beta_schedule(timesteps):
     beta_start = 0.0001
     beta_end = 0.02
     return torch.linspace(beta_start, beta_end, timesteps)
 
+
 def quadratic_beta_schedule(timesteps):
     beta_start = 0.0001
     beta_end = 0.02
     return torch.linspace(beta_start**0.5, beta_end**0.5, timesteps) ** 2
+
 
 def exp_beta_schedule(timesteps):
     beta_start = 0.0001
@@ -40,10 +43,12 @@ def exp_beta_schedule(timesteps):
     tmp_2 = (tmp - tmp_min) / (tmp_max - tmp_min)
     return tmp_2 * (beta_end - beta_start) + beta_start
 
+
 def extract(a, t, x_shape):
     batch_size = t.shape[0]
     out = a.gather(-1, t.cpu())
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
 
 def total_beta_schedule(beta_schedule, timestep, prev_betas=None):
     if prev_betas == None: 
@@ -110,13 +115,15 @@ def training(data_loader, epochs, timestep, net, optimizer:optim.Adam, device, s
     return training_loss, training_taken_step, taken_step
 
 
+
 """ Training part - Guided (MNIST) """
-def training_guided(data_loader, epochs, timestep, net, optimizer:optim.Adam, device, sqrt_alphas, sqrt_one_minus_alphas):
+def training_guided(data_loader, epochs, timestep, net, optimizer:optim.Adam, device, sqrt_alphas, sqrt_one_minus_alphas, condition_shift=1000):
     taken_step = 0
 
     training_taken_step = []
     training_loss = []
 
+    # training net with conditioning variable
     for i in range(epochs):
         print(f">> epoch : {i} / with condition variable")
         for datas, labels in data_loader: # sample a data point
@@ -127,7 +134,8 @@ def training_guided(data_loader, epochs, timestep, net, optimizer:optim.Adam, de
             datas = datas.to(device)
 
             t = torch.randint(0, timestep, (batch_size,), device=device).long()# sample a point along the Markov chain
-            c = torch.tensor(labels, device=device).long()    
+            c = torch.tensor(labels, device=device) + condition_shift
+            c = c.long()    
 
             # get alpha values
             sqrt_alphas_t = extract(sqrt_alphas, t, datas.shape)
@@ -148,38 +156,39 @@ def training_guided(data_loader, epochs, timestep, net, optimizer:optim.Adam, de
             loss.backward()
             optimizer.step()
 
-        # 코드를 줄일 수 있을거 같은데, 일단은 이렇게 만들어보자.
-        print(f">> epoch : {epochs} // without conditioning variable")
-        for datas, labels in data_loader:
-            optimizer.zero_grad()
-            
-            taken_step += 1
-            batch_size = len(labels)
-            datas = datas.to(device)
+    # training net without conditioning variable
+    print(f"training without conditioning variable")
+    for datas, labels in data_loader:
+        optimizer.zero_grad()
+        
+        taken_step += 1
+        batch_size = len(labels)
+        datas = datas.to(device)
 
-            t = torch.randint(0, timestep, (batch_size,), device=device).long()# sample a point along the Markov chain
+        t = torch.randint(0, timestep, (batch_size,), device=device).long()# sample a point along the Markov chain
 
-            # get alpha values
-            sqrt_alphas_t = extract(sqrt_alphas, t, datas.shape)
-            sqrt_one_minus_alphas_t = extract(sqrt_one_minus_alphas, t, datas.shape)
+        # get alpha values
+        sqrt_alphas_t = extract(sqrt_alphas, t, datas.shape)
+        sqrt_one_minus_alphas_t = extract(sqrt_one_minus_alphas, t, datas.shape)
 
-            epsilon = torch.randn_like(datas).to(device) # sample a noise vector
-            z_t = sqrt_alphas_t * datas + sqrt_one_minus_alphas_t * epsilon # Evaluate noisy latent variable
-            output = net(z_t, t)
-            loss = F.mse_loss(epsilon, output) # compute loss term >> change loss term into l1 loss
+        epsilon = torch.randn_like(datas).to(device) # sample a noise vector
+        z_t = sqrt_alphas_t * datas + sqrt_one_minus_alphas_t * epsilon # Evaluate noisy latent variable
+        output = net(z_t, t)
+        loss = F.mse_loss(epsilon, output) # compute loss term >> change loss term into l1 loss
 
-            if taken_step % 100 == 0:
-                print("Loss: ", loss.item())
+        if taken_step % 100 == 0:
+            print("Loss: ", loss.item())
 
-            # save loss
-            training_taken_step.append(taken_step)
-            training_loss.append(loss)
+        # save loss
+        training_taken_step.append(taken_step)
+        training_loss.append(loss)
 
-            loss.backward()
-            optimizer.step()
+        loss.backward()
+        optimizer.step()
 
 
     return training_loss, training_taken_step, taken_step
+
 
 
 """ sampling from net """
@@ -197,6 +206,7 @@ def sample(x:torch.Tensor, t, betas, sampling_1, sampling_2, net, device):
     x = mu + torch.sqrt(betas_t) * epsilon
 
     return x
+
 
 @torch.no_grad()
 def sampling(timestep, betas, sampling_1, sampling_2, net, device, noise=None):
@@ -219,64 +229,71 @@ def sampling(timestep, betas, sampling_1, sampling_2, net, device, noise=None):
 
     return x
 
+
+
 """ sampling - Guided (MNIST) """
 @torch.no_grad()
-def sample_guided(z_t:torch.Tensor, t:int, c, delta, betas, sampling_1, sampling_2, net, device):
+def sample_guided(z_t:torch.Tensor, t:int, c, delta, betas, sqrt_betas, sqrt_one_minus_alphas, net, device, condition_shift):
     # evaluate network output
     net_t = torch.tensor([t], device=device)
-    net_c = torch.tensor([c], device=device)
+    net_c = torch.tensor([c+condition_shift], device=device)
     net_c_null = None
 
-    sampling_1_t = extract(sampling_1, net_t, z_t.shape)
-    sampling_2_t = extract(sampling_2, net_t, z_t.shape)
     betas_t = extract(betas, net_t, z_t.shape)
+    sqrt_betas_t = extract(sqrt_betas, net_t, z_t.shape)
+    sqrt_one_minus_alphas_t = extract(sqrt_one_minus_alphas, net_t, z_t.shape)
 
     # sample
-    mu_1 = sampling_1_t * (z_t - sampling_2_t * net(z_t, net_t, net_c))
-    epsilon_1 = torch.randn_like(z_t).to(device)
-    z_next_1 = mu_1 + torch.sqrt(betas_t) * epsilon_1
+    epsilon = torch.randn_like(z_t).to(device)
+    score = (-1/sqrt_one_minus_alphas_t) * (delta*net(z_t, net_t, net_c) + (1-delta)*net(z_t, net_t, net_c_null))
 
-    mu_2 = sampling_1_t * (z_t - sampling_2_t * net(z_t, net_t, net_c_null))
-    epsilon_2 = torch.randn_like(z_t).to(device)
-    z_next_2 = mu_2 + torch.sqrt(betas_t) * epsilon_2
-
-    z_next = (z_next_1 * delta)*(z_next_2 * (1-delta))
+    z_next = z_t + (betas_t/2)*score + sqrt_betas_t*epsilon
 
     return z_next
 
+
 @torch.no_grad()
-def sampling_guided(timestep, condition:int, delta, betas, sampling_1, sampling_2, net, device, noise=None):
+def sampling_guided(timestep, condition:int, delta, betas, sqrt_one_minus_alphas, net, device, noise=None, condition_shift=1000):
     if noise==None:
         noise = torch.randn((1,1,32,32)) # sample from final latent space
     x = noise.to(device)
-    # sampling_1 = sampling_1.to(device)
-    # sampling_2 = sampling_2.to(device)
-    # betas = betas.to(device)
 
-    for t in reversed(range(1, timestep)):
-        x = sample_guided(x, t, condition, delta, betas, sampling_1, sampling_2, net, device)
+    sqrt_betas = torch.sqrt(betas)
 
-    last_t = torch.tensor([0], device=device)
-    last_c = torch.tensor([condition], device=device)
-    last_c_null = None
-    sampling_1_0 = extract(sampling_1, last_t, x.shape)
-    sampling_2_0 = extract(sampling_2, last_t, x.shape)
-    betas_0 = extract(betas, last_t, x.shape)
-
-    mu_1 = sampling_1_0 * (x - sampling_2_0 * net(x, last_t, last_c))
-    epsilon_1 = torch.randn_like(x).to(device)
-    z_next_1 = mu_1 + torch.sqrt(betas_0) * epsilon_1
-
-    mu_2 = sampling_1_0 * (x - sampling_2_0 * net(x, last_t, last_c_null))
-    epsilon_2 = torch.randn_like(x).to(device)
-    z_next_2 = mu_2 + torch.sqrt(betas_0) * epsilon_2
-
-    x = (z_next_1 * delta)*(z_next_2 * (1-delta))
+    for t in reversed(range(0, timestep)):
+        x = sample_guided(x, t, condition, delta, betas, sqrt_betas, sqrt_one_minus_alphas, net, device, condition_shift)
 
     return x
 
-""" sampling - DDIM """
 
+
+# exponential scheduling for tau in DDIM scheduling
+def DDIM_exp_scheduling(length, timestep):
+    y_min = 1
+    y_max = timestep
+    x = torch.linspace(-6,6,length)
+    tmp = torch.exp(x)
+    tmp_max = tmp.max()
+    tmp_min = tmp.min()
+    tmp_2 = (tmp - tmp_min) / (tmp_max - tmp_min)
+    tmp_3:torch.Tensor = tmp_2 * (y_max - y_min) + y_min
+
+    ret = []
+    for i in range(0,length):
+        
+        # debug
+        # print(int(round(tmp_3[i].tolist())))
+
+        if ret and (int(round(tmp_3[i].tolist())) <= ret[-1]):
+            ret.append(ret[-1] + 1)
+        else:
+            ret.append(int(round(tmp_3[i].tolist())))
+
+    return ret
+
+
+
+""" sampling - DDIM """
 @torch.no_grad()
 def sample_DDIM(x:torch.Tensor, 
                 t, 
@@ -292,11 +309,14 @@ def sample_DDIM(x:torch.Tensor,
     sqrt_alpha_t_minus_1 = extract(sqrt_alphas, net_t_minus_1, x.shape)
     sqrt_one_minus_alpha_t = extract(sqrt_one_minus_alphas, net_t, x.shape)
     sqrt_one_minus_alpha_t_minus_1 = extract(sqrt_one_minus_alphas, net_t_minus_1, x.shape)
+
+    net_result = net(x, net_t)
     
-    x_1 = sqrt_alpha_t_minus_1 * ((x - sqrt_one_minus_alpha_t * net(x, net_t))/sqrt_alpha_t)
-    x_2 = sqrt_one_minus_alpha_t_minus_1 * net(x, net_t)
+    x_1 = sqrt_alpha_t_minus_1 * ((x - sqrt_one_minus_alpha_t * net_result)/sqrt_alpha_t)
+    x_2 = sqrt_one_minus_alpha_t_minus_1 * net_result
 
     return x_1 + x_2
+
 
 @torch.no_grad()
 def sampling_DDIM(timestep,
@@ -311,29 +331,19 @@ def sampling_DDIM(timestep,
     if noise==None:
         noise = torch.randn((1,1,32,32)) # sample from final latent space
     x = noise.to(device)
-    # sampling_1 = sampling_1.to(device)
-    # sampling_2 = sampling_2.to(device)
-    # betas = betas.to(device)
 
     # generate sequence tau
     tau = range(1, timestep)
     if length != None:
-        tau = sorted(list(np.random.choice(range(2,timestep-1), length, replace=False)))
+        # tau = [tau_iter for tau_iter in range(1, timestep, timestep//length)] # linear scheduling
+        tau = DDIM_exp_scheduling(length, timestep-1)
 
-    # start from Terminal step
-    t = timestep - 1
-    print(f"tau : {t}")
-    x = sample_DDIM(x, t, sqrt_alphas, sqrt_one_minus_alphas, net, device)
-
+    # generating process
     for t in reversed(tau):
         t = int(t)
-        print(f"tau : {t}") # tmp
+        # print(f"tau : {t}") # tmp
         x = sample_DDIM(x, t, sqrt_alphas, sqrt_one_minus_alphas, net, device)
     
-    # end at step 1
-    t = 1
-    print(f"tau : {t}") 
-    x = sample_DDIM(x, t, sqrt_alphas, sqrt_one_minus_alphas, net, device)
 
     # end step
     t_0 = torch.tensor([0], device=device)
@@ -353,6 +363,9 @@ def sampling_DDIM(timestep,
 # betas are saved in "./checkpoints/{net_name}_betas.pt"
 def save_net_betas(net:torch.nn.Module, 
                    betas:torch.Tensor,
+                   alphas:torch.Tensor,
+                   sqrt_alphas:torch.Tensor,
+                   sqrt_one_minus_alphas:torch.Tensor,
                    sampling_1:torch.Tensor,
                    sampling_2:torch.Tensor, 
                    net_name="default"):
@@ -365,13 +378,17 @@ def save_net_betas(net:torch.nn.Module,
 
     # save betas
     torch.save({'betas':betas,
+                'alphas':alphas,
+                'sqrt_alphas':sqrt_alphas,
+                'sqrt_one_minus_alphas':sqrt_one_minus_alphas,
                 'sampling_1':sampling_1,
                 'sampling_2':sampling_2}, save_betas_path)
 
 
 def load_net_betas(net:torch.nn.Module,
                    device,
-                   net_name="default"):
+                   net_name="default",
+                   method="DDPM"):
     
     save_net_path = f"./checkpoints/{net_name}_net.pt"
     save_betas_path = f"./checkpoints/{net_name}_betas.pt"
@@ -381,10 +398,23 @@ def load_net_betas(net:torch.nn.Module,
     net.eval()
     net.to(device)
 
-    # load betas
     loaded_schedule = torch.load(save_betas_path)
-    loaded_betas = loaded_schedule['betas']
-    loaded_sampling_1 = loaded_schedule['sampling_1']
-    loaded_sampling_2 = loaded_schedule['sampling_2']
 
-    return net, loaded_betas, loaded_sampling_1, loaded_sampling_2
+    # load betas - DDPM
+    if method == "DDPM":    
+        loaded_betas = loaded_schedule['betas']
+        loaded_sampling_1 = loaded_schedule['sampling_1']
+        loaded_sampling_2 = loaded_schedule['sampling_2']
+
+        return net, loaded_betas, loaded_sampling_1, loaded_sampling_2
+    elif method == "DDIM":
+        loaded_alphas = loaded_schedule['alphas']
+        loaded_sqrt_alphas = loaded_schedule['sqrt_alphas']
+        loaded_sqrt_one_minus_alphas = loaded_schedule['sqrt_one_minus_alphas']
+        return net, loaded_alphas, loaded_sqrt_alphas, loaded_sqrt_one_minus_alphas
+    
+    elif method == "guided":
+        loaded_betas = loaded_schedule['betas']
+        loaded_sqrt_one_minus_alphas = loaded_schedule['sqrt_one_minus_alphas']
+        return net, loaded_betas, loaded_sqrt_one_minus_alphas
+    
--- README.md
+++ README.md
@@ -2,28 +2,39 @@
 
 Learning Diffusion Model(DDPM) and basics of PyTorch to code DDPM
 
-## pytorch learning jupyter notebooks based on [차근차근 실습하며 배우는 파이토치 딥러닝 프로그래밍](https://github.com/wikibook/pytorchdl2)
-  - [download & use MNIST dataset in pytorch](/pytorch_learning_mnist.ipynb)
-  - [how to use CNN in pytorch](/pytorch_learning_CNN.ipynb) 
+## Environment Required
 
-## Diffusion model coding
-  - [forward process - using MNIST](/forward_process.ipynb)
+- python 3.8
+- PyTorch 2.3.1
+- cuda 11.8
+
+## Codes
+
+### pytorch learning jupyter notebooks based on [차근차근 실습하며 배우는 파이토치 딥러닝 프로그래밍](https://github.com/wikibook/pytorchdl2)
+  - [pytorch_learning_mnist.ipynb](/pytorch_learning_mnist.ipynb) : download & use MNIST dataset in pytorch
+  - [pytorch_learning_CNN.ipynb](/pytorch_learning_CNN.ipynb) : how to use CNN in pytorch
+
+### Diffusion model coding
+  - [forward_process.ipynb](/forward_process.ipynb) : forward process - using MNIST
   
   - Learning U-net & diffusion model with pytorch 
-    - [referenced website](https://huggingface.co/blog/annotated-diffusion)
-    - [U-net & Diffusion model Learning](./Unet_learning.ipynb)
-    - [U-net as complete module](./Unet.py)
+    - [https://huggingface.co/blog/annotated-diffusion](https://huggingface.co/blog/annotated-diffusion) : referenced website
+    - [Unet_learning.ipynb](./Unet_learning.ipynb) : U-net & Diffusion model Learning
+    - [Unet.py](./Unet.py) : U-net as complete module
   
-  - [Diffusion model using MNIST dataset](/diffusion_model.ipynb)
+  - [diffusion_model.ipynb](/diffusion_model.ipynb) : Diffusion model training & sampling using MNIST dataset
   
-  - [Diffusion model as complete module - containing Guided sampling, DDIM sampling](./DiffusionModel.py)
+  - [DiffusionModel.py](./DiffusionModel.py) : Diffusion model as complete module - containing Guided sampling, DDIM sampling
   
-  - [Guided Diffusion model](./guided_diffusion.ipynb)
-    - [GuidedUnet - modified Unet to accept integer conditioning variable](./GuidedUnet.py)
-    - [Guided Diffusion model only inference](./guided_diffusion_only_inference.ipynb)
+  - Guided Diffusion model coding
+    - [guided_diffusion_training.ipynb](./guided_diffusion_training.ipynb) : Guided Diffusion model training
+    - [GuidedUnet.py](./GuidedUnet.py) : GuidedUnet - modified Unet to accept integer conditioning variable
+    - [guided_diffusion_sampling.ipynb](./guided_diffusion_sampling.ipynb) : Guided Diffusion model sampling
   
-  - [DDIM sampling from DDPM model](./DDIM_sampling.ipynb)
+  - DDIM coding
+    - [DDIM_training.ipynb](./DDIM_training.ipynb) : train model for DDIM sampling
+    - [DDIM_sampling.ipynb](./DDIM_sampling.ipynb) : DDIM sampling from DDPM model
 
-## Paper reviews
+### Paper reviews
   - [Diffusion Model](./diffusion_presentation.pdf)
-  - [DDIM paper](./DDIM_발표_추성재.pdf)
+  - [DDIM paper](./DDIM_presentation.pdf)
